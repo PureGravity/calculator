2eb16cf94373d37f5fbf12b9832579ff902f930c
Jay <49499730+PureGravity@users.noreply.github.com>
2024-08-16 00:58:56 -0400

    Create initial code

---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.querySelector('.button')
+let buttons = document.querySelector('.buttons')
+let y = 4
+let x = 4
+
+for (let i = 0; i < x; i++) {
+    let newButton = button.cloneNode(true)
+
+}
