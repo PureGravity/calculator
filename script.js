852c7b05126bdc3c3e416452b7c27e0a4e7f5705
Jay <49499730+PureGravity@users.noreply.github.com>
2024-08-18 18:20:23 -0400

    Add eventlistener for button clicks

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let button = document.querySelector('.button')
-let buttons = document.querySelector('.buttons')
-let y = 4
-let x = 4
+// let buttons = Array.from(document.querySelectorAll('button'))
 
-for (let i = 0; i < x; i++) {
-    let newButton = button.cloneNode(true)
+// buttons.forEach(function(button) {
+//     button.addEventListener('click', function(){
+//         button.style.backgroundColor = 'black';
+//     })
+// })
 
-}
+let buttons = document.querySelectorAll('.button');
+
+buttons.forEach(function(button) {
+    button.addEventListener('mousedown', function() {
+        button.style.backgroundColor = 'black';
+    });
+
+    button.addEventListener('mouseup', function() {
+        button.style.backgroundColor = '';
+    });
+});
