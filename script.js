a31da2077b3079b49aaa9ea3da92cddd664fbd61
Jay <49499730+PureGravity@users.noreply.github.com>
2024-09-10 01:28:58 -0400

    Reformat script for buttons

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
-let one
-
 let firstNum;
 let operator;
 let lastNum;
 
 let buttons = Array.from(document.querySelectorAll('.button'));
 let digit = Array.from(document.querySelectorAll('.digit'));
-let equals = document.querySelector('.equalSign');
 let screenDisplay = document.querySelector('.screenDisplay');
 
-digit.forEach(function(button) {
-    button.addEventListener('mousedown', function() {
-        button.style.backgroundColor = 'black';
-    });
-
-    button.addEventListener('mouseup', function() {
-        button.style.backgroundColor = '';
-    });
-});
-
-buttons.forEach(function(button) {
-    button.addEventListener('mousedown', function() {
-        button.style.backgroundColor = 'black';
-    });
-
-    button.addEventListener('mouseup', function() {
-        button.style.backgroundColor = '';
-    });
-});
-
-equals.addEventListener('mousedown', function(){
-    equals.style.backgroundColor = 'black';
-});
-equals.addEventListener('mouseup', function(){
-    equals.style.backgroundColor = '';
-});
-
+// visual feedback for buttons
 digit.forEach(button => {
+    button.addEventListener('mousedown', function() {
+        button.style.backgroundColor = 'black';
+    });
+
+    button.addEventListener('mouseup', function() {
+        button.style.backgroundColor = '';
+    });
+
     button.addEventListener('click', function() {
         const digit = parseInt(button.textContent);
         screenDisplay.textContent = parseInt(screenDisplay.textContent) === 0 ? digit : parseInt(screenDisplay.textContent) * 10 + digit;
     });
 });
 
-buttons[5].addEventListener('click', function() {
-    screenDisplay.textContent = 0
-})
+buttons.forEach(button => {
+    button.addEventListener('mousedown', function() {
+        button.style.backgroundColor = 'black';
+    });
+
+    button.addEventListener('mouseup', function() {
+        button.style.backgroundColor = '';
+    });
+});
+
+// CE and DEL button functionality
+buttons.find(button => button.textContent === 'CE').addEventListener('click', function() {
+    screenDisplay.textContent = '0';
+});
+
+buttons.find(button => button.textContent === 'DEL').addEventListener('click', function() {
+    let currentDisplay = screenDisplay.textContent;
+    
+    screenDisplay.textContent = currentDisplay.length > 1 ? currentDisplay.slice(0, -1) : '0';
+});
+
+// operator button functionality
