2dc24b696202986317b091960c06779da6ead44a
Jay <49499730+PureGravity@users.noreply.github.com>
2024-09-10 00:52:31 -0400

    Modify script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-// let buttons = Array.from(document.querySelectorAll('button'))
+let one
 
-// buttons.forEach(function(button) {
-//     button.addEventListener('click', function(){
-//         button.style.backgroundColor = 'black';
-//     })
-// })
+let firstNum;
+let operator;
+let lastNum;
 
-let buttons = document.querySelectorAll('.button');
+let buttons = Array.from(document.querySelectorAll('.button'));
+let digit = Array.from(document.querySelectorAll('.digit'));
+let equals = document.querySelector('.equalSign');
+let screenDisplay = document.querySelector('.screenDisplay');
+
+digit.forEach(function(button) {
+    button.addEventListener('mousedown', function() {
+        button.style.backgroundColor = 'black';
+    });
+
+    button.addEventListener('mouseup', function() {
+        button.style.backgroundColor = '';
+    });
+});
 
 buttons.forEach(function(button) {
     button.addEventListener('mousedown', function() {
@@ -17,3 +28,21 @@ buttons.forEach(function(button) {
         button.style.backgroundColor = '';
     });
 });
+
+equals.addEventListener('mousedown', function(){
+    equals.style.backgroundColor = 'black';
+});
+equals.addEventListener('mouseup', function(){
+    equals.style.backgroundColor = '';
+});
+
+digit.forEach(button => {
+    button.addEventListener('click', function() {
+        const digit = parseInt(button.textContent);
+        screenDisplay.textContent = parseInt(screenDisplay.textContent) === 0 ? digit : parseInt(screenDisplay.textContent) * 10 + digit;
+    });
+});
+
+buttons[5].addEventListener('click', function() {
+    screenDisplay.textContent = 0
+})
