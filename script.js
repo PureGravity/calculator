117e8e197fd53dd76e68cfbf583cb07f9d94164d
Jay <49499730+PureGravity@users.noreply.github.com>
2024-09-15 00:48:41 -0400

    Feat: add operator button functionality

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ let firstNum;
 let operator;
 let lastNum;
 
+let calculate = document.querySelector('#equal')
+let answer = document.querySelector('.answer')
+
+let operators = Array.from(document.querySelectorAll('.operator'))
 let buttons = Array.from(document.querySelectorAll('.button'));
 let digit = Array.from(document.querySelectorAll('.digit'));
+
 let screenDisplay = document.querySelector('.screenDisplay');
 
 // visual feedback for buttons
@@ -11,14 +16,13 @@ digit.forEach(button => {
     button.addEventListener('mousedown', function() {
         button.style.backgroundColor = 'black';
     });
-
     button.addEventListener('mouseup', function() {
         button.style.backgroundColor = '';
     });
-
     button.addEventListener('click', function() {
         const digit = parseInt(button.textContent);
         screenDisplay.textContent = parseInt(screenDisplay.textContent) === 0 ? digit : parseInt(screenDisplay.textContent) * 10 + digit;
+
     });
 });
 
@@ -26,7 +30,15 @@ buttons.forEach(button => {
     button.addEventListener('mousedown', function() {
         button.style.backgroundColor = 'black';
     });
+    button.addEventListener('mouseup', function() {
+        button.style.backgroundColor = '';
+    });
+});
 
+operators.forEach(button => {
+    button.addEventListener('mousedown', function() {
+        button.style.backgroundColor = 'black';
+    });
     button.addEventListener('mouseup', function() {
         button.style.backgroundColor = '';
     });
@@ -36,11 +48,45 @@ buttons.forEach(button => {
 buttons.find(button => button.textContent === 'CE').addEventListener('click', function() {
     screenDisplay.textContent = '0';
 });
-
 buttons.find(button => button.textContent === 'DEL').addEventListener('click', function() {
     let currentDisplay = screenDisplay.textContent;
-    
     screenDisplay.textContent = currentDisplay.length > 1 ? currentDisplay.slice(0, -1) : '0';
 });
 
 // operator button functionality
+
+function addition(x, y) {
+    answer.textContent = x + y;
+};
+function subtraction(x, y) {
+    console.log(x - y);
+};
+function multiplication(x, y) {
+    console.log(x * y);
+};
+function division(x, y) {
+    console.log(x / y);
+};
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            addition(num1, num2)
+            break;
+        case '-':
+            subtraction(num1, num2)
+            break;
+        case '*':
+            multiplication(num1, num2)
+            break;
+        case '/':
+            division(num1, num2)
+            break;
+        
+    }
+}
+
+calculate.addEventListener('click', function() {
+    // operate(firstNum, operator, lastNum)
+    operate(7, '+', 4)    
+})
